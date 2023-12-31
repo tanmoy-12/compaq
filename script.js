fb119c58577bbf258beff5e34e5bf89cf294e455
tanmoy-12 <tanmoy.chanda@outlook.com>
2023-12-31 11:01:17 +0530

    data update

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const certificate_course = [
     { course_name: 'School Byte', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
-    { course_name: 'MS-Office', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
-    { course_name: 'FA', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
-    { course_name: 'DFA', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
-    { course_name: 'DDTP', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
-    { course_name: 'DCA', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
-    { course_name: ';Language', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
-    { course_name: 'Multimedia(Basic)', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
-    { course_name: 'Internet', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
-    { course_name: 'ADCA', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' }
+    { course_name: 'MS-Office', course_details: 'Basic of Windows, MS-Word, MS-Powerpoint, MS-Excel, MS-Access', course_duration: '4 months' },
+    { course_name: 'FA', course_details: 'MS-Word, MS-Excel, Tally 9.0', course_duration: '6 months' },
+    { course_name: 'DFA', course_details: 'MS-Word, MS-Excel, Tally 9.0, Expert, Ace', course_duration: '8 months' },
+    { course_name: 'DDTP', course_details: 'PageMaker, Corel Draw, Corel Photo Paint, Photoshop', course_duration: '6 months' },
+    { course_name: 'DCA', course_details: 'Windows, Windows Explorer, Command Prompt, MS-Word, MS-Powerpoint, MS-Excel, MS-Access', course_duration: '6 months' },
+    { course_name: 'Language', course_details: 'C++, Foxpro(Database)', course_duration: '3 months' },
+    { course_name: 'Multimedia(Basic)', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '3 months' },
+    { course_name: 'ADCA', course_details: 'Windows, Windows Explorer, Command Prompt, MS-Word, MS-Powerpoint, MS-Excel, MS-Access, Visual Basic, HTML, Foxpro', course_duration: '12 months' }
 ];
 
 const coursesContainer = document.querySelector('.courses');
@@ -25,21 +24,21 @@ certificate_course.forEach((certificate_course) => {
 });
 
 
-const non_certificate_course = [
-    { course_name: 'School Byte', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
-    { course_name: 'School Byte', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
-    { course_name: 'School Byte', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' }
-];
+// const non_certificate_course = [
+//     { course_name: 'School Byte', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
+//     { course_name: 'School Byte', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' },
+//     { course_name: 'School Byte', course_details: 'Course Curriculum for (ICSE/CBSE & WBBSE), Logo, Windows, MS-Word, MS-PowerPoint, MS-Excel, HTML', course_duration: '12 months' }
+// ];
 
-const non_coursesContainer = document.querySelector('.ncourses');
+// const non_coursesContainer = document.querySelector('.ncourses');
 
-non_certificate_course.forEach((non_certificate_course) => {
-    const ncourse_container = document.createElement('div');
-    ncourse_container.classList.add('non-certificate-course-container');
-    ncourse_container.innerHTML = `
-        <h2>${non_certificate_course.course_name}</h2>
-        <p>${non_certificate_course.course_details}</p>
-        <h4>${non_certificate_course.course_duration}</h4>
-    `;
-    non_coursesContainer.appendChild(ncourse_container);
-});
+// non_certificate_course.forEach((non_certificate_course) => {
+//     const ncourse_container = document.createElement('div');
+//     ncourse_container.classList.add('non-certificate-course-container');
+//     ncourse_container.innerHTML = `
+//         <h2>${non_certificate_course.course_name}</h2>
+//         <p>${non_certificate_course.course_details}</p>
+//         <h4>${non_certificate_course.course_duration}</h4>
+//     `;
+//     non_coursesContainer.appendChild(ncourse_container);
+// });
